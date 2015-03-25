b014dce7d8f5713d0acc8eee36c132b3f94f7aca
Steve Russo <svrusso1@gmail.com>
2015-03-25 10:21:36 -0400

    Add comments

--- start.sh
+++ start.sh
@@ -17,9 +17,20 @@ fi
 
 xhost +local:
 
+#Enumerate the sound devices in /dev/snd
 SNDDEVS=$(find /dev/snd -type c)
+
+#Enumerate DRI devices in /dev/dri. See:
+#https://www.stgraber.org/2014/02/09/lxc-1-0-gui-in-containers/
+#This is needed for WebGL to be able to access the video.
 SNDDEVS+=" $(find /dev/dri -type c)"
+
 SNDFLAGS=$(j=""; for i in $SNDDEVS; do j+="--device=\"$i:$i\" "; done; echo $j)
+
+#	Note
+#	--lxc-conf='lxc.cgroup.devices.allow = c 116:* rwm' \ #/dev/snd devices
+#	--lxc-conf='lxc.cgroup.devices.allow = c 226:* rwm' \ #/dev/dri devices
+#	grants LXC access to /dev/snd and /dev/dri
 
 $SUDO \
 	docker run $FLAGS -i --rm \
